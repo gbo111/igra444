33f255c3d8bf1a47dfcd90baeefdc43a221790a0
gbo111 <alesgibicar@yahoo.com>
2022-04-23 22:47:56 +0200

    1-3

--- index.js
+++ index.js
@@ -77,50 +77,50 @@ app.post('/api', (request, response) => {
 
 
 //     databaseEasyAlldata.insert([
-//     {"timeX":"23","time":"0min : 23sec","moves":12,"name":"Rok Hočevar","nick":"Rokec","sola":"Šcnm","mail":"rhocevar6rok23452345@gmail.com","gdpr":"on","timeStamp":"1650617844","_id":"DkeZNhLeqt2jJuye"},
-//     {"timeX":"20","time":"0min : 20sec","moves":12,"name":"Nejc Korošec","nick":"Nejko","sola":"SPSŠ ZREČE","mail":"nejc.korosec21@gmail.com","gdpr":"on","mailing":"on","timeStamp":"1650168961","_id":"NuoML3xECugNG1wQ"},
-//     {"timeX":"21","time":"0min : 21sec","moves":14,"name":"Lukas Lešnjak Pelko","nick":"Motorhead","sola":"ŠCNM","mail":"vy5k45@gmail.com","gdpr":"on","mailing":"on","timeStamp":"1650617503","_id":"fafIsrJ9XRZjtuVx"}
+//     {"timeX":"23","time":"0min : 23sec","moves":12,"name":"Rok Hočevar","nick":"Rokec","sola":"Šcnm","mail":"rhocevar6rok23452345@gmail.com","gdpr":"on","timeStamp":"1650617844"},
+//     {"timeX":"20","time":"0min : 20sec","moves":12,"name":"Nejc Korošec","nick":"Nejko","sola":"SPSŠ ZREČE","mail":"nejc.korosec21@gmail.com","gdpr":"on","mailing":"on","timeStamp":"1650168961"},
+//     {"timeX":"21","time":"0min : 21sec","moves":14,"name":"Lukas Lešnjak Pelko","nick":"Motorhead","sola":"ŠCNM","mail":"vy5k45@gmail.com","gdpr":"on","mailing":"on","timeStamp":"1650617503"}
 // ], function (err, newDocs) {
 //     });
 //
 //     databaseEasy.insert([
-//       {"timeX":"23","time":"0min : 23sec","moves":12,"nick":"Rokec","_id":"0dB24smsyAei8QmT"},
-//       {"timeX":"21","time":"0min : 21sec","moves":14,"nick":"Motorhead","_id":"15z1iXQaRZS3Ds6V"},
-//       {"timeX":"20","time":"0min : 20sec","moves":12,"nick":"Nejko","_id":"P1k3LefnFkVjodcX"}
+//       {"timeX":"23","time":"0min : 23sec","moves":12,"nick":"Rokec"},
+//       {"timeX":"21","time":"0min : 21sec","moves":14,"nick":"Motorhead"},
+//       {"timeX":"20","time":"0min : 20sec","moves":12,"nick":"Nejko"}
 // ], function (err, newDocs) {
 //     });
 //
 //
 //
 //     databaseNormalAlldata.insert([
-//       {"timeX":"44","time":"0min : 44sec","moves":25,"name":"Nick Bohorč","nick":"Bohorč","sola":"SCNM","mail":"nickbohorc33@gmail.com","gdpr":"on","mailing":"on","timeStamp":"1650617567","_id":"8IfMe94Fe8qoeaEY"},
-//       {"timeX":"38","time":"0min : 38sec","moves":18,"name":"Domen Janežič","nick":"Domen","sola":"Srednja šola tehničnih strok šiška","mail":"domen.janezic11@gmail.com","gdpr":"on","mailing":"on","timeStamp":"1650183180","_id":"8bvh5NpALYvRaoT3"},
-//       {"timeX":"38","time":"0min : 38sec","moves":27,"name":"Aleks Smolič","nick":"Smolič","sola":"Mehatronika","mail":"aleks.smolic@gmail.com","gdpr":"on","mailing":"on","timeStamp":"1650617187","_id":"wC9qqr4fxdtmraNk"}
+//       {"timeX":"44","time":"0min : 44sec","moves":25,"name":"Nick Bohorč","nick":"Bohorč","sola":"SCNM","mail":"nickbohorc33@gmail.com","gdpr":"on","mailing":"on","timeStamp":"1650617567"},
+//       {"timeX":"38","time":"0min : 38sec","moves":18,"name":"Domen Janežič","nick":"Domen","sola":"Srednja šola tehničnih strok šiška","mail":"domen.janezic11@gmail.com","gdpr":"on","mailing":"on","timeStamp":"1650183180"},
+//       {"timeX":"38","time":"0min : 38sec","moves":27,"name":"Aleks Smolič","nick":"Smolič","sola":"Mehatronika","mail":"aleks.smolic@gmail.com","gdpr":"on","mailing":"on","timeStamp":"1650617187"}
 //
 // ], function (err, newDocs) {
 //     });
 //
 //     databaseNormal.insert([
-//       {"timeX":"44","time":"0min : 44sec","moves":25,"nick":"Bohorč","_id":"ZkxcSln4Y7Ry0O42"},
-//       {"timeX":"38","time":"0min : 38sec","moves":27,"nick":"Smolič","_id":"gYN8eJKZXVak3ehW"},
-//       {"timeX":"38","time":"0min : 38sec","moves":18,"nick":"Domen","_id":"v6V58YMq0XVRasRu"}
+//       {"timeX":"44","time":"0min : 44sec","moves":25,"nick":"Bohorč"},
+//       {"timeX":"38","time":"0min : 38sec","moves":27,"nick":"Smolič"},
+//       {"timeX":"38","time":"0min : 38sec","moves":18,"nick":"Domen"}
 // ], function (err, newDocs) {
 //     });
 //
 //
 //
 //     databaseHardAlldata.insert([
-//       {"timeX":"73","time":"1min : 13sec","moves":34,"name":"Domen Janežič","nick":"Domen","sola":"Srednja šola tehničnih strok šiška","mail":"domen.janezic11@gmail.com","gdpr":"on","mailing":"on","timeStamp":"1650043220","_id":"Fs0xcE8PjFiF4sC8"},
-//       {"timeX":"57","time":"0min : 57sec","moves":32,"name":"Luka Cvitko","nick":"lukac","sola":"SCNM","mail":"lukacvitko005@gmail.com","gdpr":"on","timeStamp":"1650365414","_id":"NlgTl7B9S08vlQq6"},
-//       {"timeX":"73","time":"1min : 13sec","moves":38,"name":"Luka cvitko","nick":"luka","sola":"SCNM ","mail":"lukacvitko005@gmail.com","gdpr":"on","timeStamp":"1650611147","_id":"ini7Hu9RyinKd9Jm"}
+//       {"timeX":"73","time":"1min : 13sec","moves":34,"name":"Domen Janežič","nick":"Domen","sola":"Srednja šola tehničnih strok šiška","mail":"domen.janezic11@gmail.com","gdpr":"on","mailing":"on","timeStamp":"1650043220"},
+//       {"timeX":"57","time":"0min : 57sec","moves":32,"name":"Luka Cvitko","nick":"lukac","sola":"SCNM","mail":"lukacvitko005@gmail.com","gdpr":"on","timeStamp":"1650365414"},
+//       {"timeX":"73","time":"1min : 13sec","moves":38,"name":"Luka cvitko","nick":"luka","sola":"SCNM ","mail":"lukacvitko005@gmail.com","gdpr":"on","timeStamp":"1650611147"}
 //
 // ], function (err, newDocs) {
 //     });
 //
 //     databaseHard.insert([
-//       {"timeX":"73","time":"1min : 13sec","moves":34,"nick":"Domen","_id":"0LTzwKnIGAYnKtJE"},
-//       {"timeX":"73","time":"1min : 13sec","moves":38,"nick":"luka","_id":"hlTG6xtg4onfTta2"},
-//       {"timeX":"57","time":"0min : 57sec","moves":32,"nick":"lukac","_id":"k1RaarSwtAv9Akj6"}
+//       {"timeX":"73","time":"1min : 13sec","moves":34,"nick":"Domen"},
+//       {"timeX":"73","time":"1min : 13sec","moves":38,"nick":"luka"},
+//       {"timeX":"57","time":"0min : 57sec","moves":32,"nick":"lukac"}
 // ], function (err, newDocs) {
 //     });
 
